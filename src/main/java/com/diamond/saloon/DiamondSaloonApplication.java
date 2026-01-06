9155508a21c3172a34379f6e4d6592c471d9c01b
Kiran Gavhane <gavhanekiran0403@gmail.com>
2026-01-06 12:35:37 +0530

    First commit

---
+++ src/main/java/com/diamond/saloon/DiamondSaloonApplication.java
@@ -0,0 +1,13 @@
+package com.diamond.saloon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiamondSaloonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiamondSaloonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diamond/saloon/DiamondSaloonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diamond.saloon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiamondSaloonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
